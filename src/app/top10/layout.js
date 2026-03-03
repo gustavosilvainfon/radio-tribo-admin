b1b5d05a811d2f23a65c17ec9c714bf4eac82d40
Gustavo Silva <gustavosilvainfon@gmail.com>
2026-03-03 14:09:46 -0400

    fix: aplicar layout com Sidebar e Header em programacao, promocoes e top10

--- src/app/top10/layout.js
+++ src/app/top10/layout.js
@@ -1,4 +1,16 @@
-export default function Top10Layout({ children }) {
-  return children;
-}
+import Sidebar from '@/components/Sidebar'
+import Header from '@/components/Header'
 
+export default function Top10Layout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
